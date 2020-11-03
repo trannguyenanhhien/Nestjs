075f747264611e5cd2e08d4db2c35bd64804dd5e
trannguyenanhhien <trannguyenanhhien@gmail.com>
2020-11-03 18:41:36 +0700

    1st

---
+++ src/auth/dto/auth-credentials.dto.ts
@@ -0,0 +1,16 @@
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
+
+export class AuthCredentialsDto {
+  @IsString()
+  @MaxLength(20)
+  @MinLength(4)
+  username: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: `password's too weak`,
+  })
+  password: string;
+}
---
+++ src/auth/get-user.decorator.ts
@@ -0,0 +1,17 @@
+// import { createParamDecorator } from '@nestjs/common';
+// import { User } from './user.entity';
+
+// export const GetUser = createParamDecorator(
+//   (data, req): User => {
+//     return req;
+//   },
+// );
+
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const GetUser = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest();
+    return request.user;
+  },
+);
